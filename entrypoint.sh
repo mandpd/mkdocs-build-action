f4a3eb8b69bbb67717d06ab052e2c32c2184d6a1
Mark Smith <mark@perfectfourth.com>
2020-10-21 10:14:38 -0700

    added docker build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,27 @@
-#!/bin/sh -l
+#!/bin/sh
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+set -x # verbose mode
+set -e # stop executing after error
+
+echo "Starting mkdocs build"
+
+####################################################
+# Set workspace permissions
+####################################################
+
+chmod -R a+w /github/workspace
+
+####################################################
+# Build the Mkdocs site
+####################################################
+
+# pip install -r requirements.txt
+# pip install mkdocs-minify-plugin>=0.2
+
+mkdocs build
+
+####################################################
+# Build completed
+####################################################
+
+echo "Completed mkdocs build"
