4427922216ad04b934f395f5da18d95e2ea2beca
Mark Smith <mark@perfectfourth.com>
2020-11-03 12:36:17 -0800

    Added pwd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -x # verbose mode
-set -e # stop executing after error
+# set -e # stop executing after error
 
 echo "Starting mkdocs build"
 
@@ -9,12 +9,11 @@ echo "Starting mkdocs build"
 # Build the Mkdocs site
 ####################################################
 
-mkdocs build --site-dir /site
+mkdocs build
+pwd
 
 ####################################################
 # Build completed
 ####################################################
 
 echo "Completed mkdocs build"
-
-pwd
